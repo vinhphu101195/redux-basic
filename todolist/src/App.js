cce7448906d4d19ff6db11dd14ae7ff7f2f561e9
PhuChau <vinhphu101195@gmail.com>
2019-07-30 14:05:49 +0300

    add redux

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { combineReducers } from "redux";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const todo = (state, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ];
+    case "TOGGLE_TODO":
+      if (state.id !== action.id) {
+        return state;
+      } else {
+        return { ...state, completed: !state.completed };
+      }
+
+    default:
+      return state;
+  }
+};
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [...state, todo(undefined, action)];
+    case "TOGGLE_TODO":
+      return state.map(el => {
+        return todo(el, action);
+      });
+    default:
+      return state;
+  }
+};
+
+const visibilityFilter = (state = "SHOW_ALL", action) => {
+  switch (action.type) {
+    case "SET_VISIBILITY_FILTER":
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
+export const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+});
+
+let nextTodoId = 0;
+
+export default class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            store.dispatch({
+              type: "ADD_TODO",
+              text: "TEST",
+              id: nextTodoId++
+            });
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Add Todo
+        </button>
+        <ul>
+          {this.props.todos.map(todo => {
+            return <li key={todo.id}>{todo.text}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
-
-export default App;
